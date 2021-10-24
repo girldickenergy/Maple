4cbef363ab833b7501849c525e3f24752a712797
Azuki <theorybreakerhacks@gmail.com>
2021-10-24 15:19:46 +0200

    Auto stash before merge of "main" and "origin/main"

--- Maple/Sdk/Player/HitObjectManager.cpp
+++ Maple/Sdk/Player/HitObjectManager.cpp
@@ -12,6 +12,10 @@ void HitObjectManager::Initialize()
 	preEmptField = RawHitObjectManager["PreEmpt"].Field;
 	preEmptSliderCompleteField = RawHitObjectManager["PreEmptSliderComplete"].Field;
 	activeModsField = RawHitObjectManager["ActiveMods"].Field;
+	hitWindow300Field = RawHitObjectManager["HitWindow300"].Field;
+	hitWindow100Field = RawHitObjectManager["HitWindow100"].Field;
+	hitWindow50Field = RawHitObjectManager["HitWindow50"].Field;
+	hitObjectRadiusField = RawHitObjectManager["HitObjectRadius"].Field;
 
 	obfuscatedType = activeModsField.GetTypeUnsafe();
 
@@ -20,6 +24,9 @@ void HitObjectManager::Initialize()
 
 	obfuscatedType["set_Value"].Method.Compile();
 	obfuscatedSetValue = static_cast<fnObfuscatedSetValue>(obfuscatedType["set_Value"].Method.GetNativeStart());
+
+	RawHitObjectManager["GetObject"].Method.Compile();
+	getObject = static_cast<fnGetObject>(RawHitObjectManager["GetObject"].Method.GetNativeStart());
 }
 
 void* HitObjectManager::Instance()
@@ -59,6 +66,43 @@ void HitObjectManager::SetActiveMods(Mods mods)
 	void* activeModsInstance = *static_cast<void**>(activeModsField.GetAddress(Instance()));
 
 	obfuscatedSetValue(activeModsInstance, mods);
+}
+
+int HitObjectManager::GetHitWindow300()
+{
+	return *static_cast<int*>(hitWindow300Field.GetAddress(Instance()));
+}
+
+int HitObjectManager::GetHitWindow100()
+{
+	return *static_cast<int*>(hitWindow100Field.GetAddress(Instance()));
+}
+
+int HitObjectManager::GetHitWindow50()
+{
+	return *static_cast<int*>(hitWindow50Field.GetAddress(Instance()));
+}
+
+float HitObjectManager::GetHitObjectRadius()
+{
+	return *static_cast<float*>(hitObjectRadiusField.GetAddress(Instance()));
+}
+
+int HitObjectManager::GetCurrentHitObjectIndex()
+{
+	return *static_cast<int*>(currentHitObjectIndexField.GetAddress(Instance()));
+}
+
+int HitObjectManager::GetHitObjectsCount()
+{
+	return *static_cast<int*>(hitObjectsCountField.GetAddress(Instance()));
+}
+
+HitObject HitObjectManager::GetHitObject(int index)
+{
+	uintptr_t hitObjectInstance = getObject(index);
+
+	HitObjectType type = *static_cast<HitObjectType*>(hitObjectInstace + )
 }
 
 double HitObjectManager::MapDifficultyRange(double difficulty, double min, double mid, double max, bool adjustToMods)
--- Maple/Sdk/Player/HitObjectManager.h
+++ Maple/Sdk/Player/HitObjectManager.h
@@ -3,6 +3,7 @@
 #include <Explorer/TypeExplorer.h>
 
 #include "../Mods/ModManager.h"
+#include "HitObject.h"
 
 class HitObjectManager
 {
@@ -10,6 +11,12 @@ class HitObjectManager
 	static inline Field preEmptField;
 	static inline Field preEmptSliderCompleteField;
 	static inline Field activeModsField;
+	static inline Field hitWindow300Field;
+	static inline Field hitWindow100Field;
+	static inline Field hitWindow50Field;
+	static inline Field hitObjectRadiusField;
+	static inline Field currentHitObjectIndexField;
+	static inline Field hitObjectsCountField;
 	
 	static inline TypeExplorer obfuscatedType;
 
@@ -18,6 +25,9 @@ class HitObjectManager
 	
 	typedef void(__fastcall* fnObfuscatedSetValue)(void* instance, Mods value);
 	static inline fnObfuscatedSetValue obfuscatedSetValue;
+
+	typedef uintptr_t(__fastcall* fnGetObject)(int index);
+	static inline fnGetObject getObject;
 public:
 	static inline TypeExplorer RawHitObjectManager;
 	
@@ -29,5 +39,12 @@ public:
 	static void SetPreEmptSliderComplete(int preEmptSliderComplete);
 	static Mods GetActiveMods();
 	static void SetActiveMods(Mods mods);
+	static int GetHitWindow300();
+	static int GetHitWindow100();
+	static int GetHitWindow50();
+	static float GetHitObjectRadius();
+	static int GetCurrentHitObjectIndex();
+	static int GetHitObjectsCount();
+	HitObject GetHitObject(int index);
 	static double MapDifficultyRange(double difficulty, double min, double mid, double max, bool adjustToMods);
 };
