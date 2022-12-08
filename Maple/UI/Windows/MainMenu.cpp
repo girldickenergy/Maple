b6e0a6cc84689a9c4f57fbfc839b6be77852c970
mrflashstudio <mrflashstudio@gmail.com>
2022-12-08 17:40:34 +0300

    update player offsets

--- Maple/UI/Windows/MainMenu.cpp
+++ Maple/UI/Windows/MainMenu.cpp
@@ -171,9 +171,9 @@ void MainMenu::Render()
                 ImGui::PopFont();
 
                 ImGui::PushFont(StyleProvider::FontSmall);
-                const ImVec2 buildStringSize = ImGui::CalcTextSize(xorstr_("mlo-30112022"));
+                const ImVec2 buildStringSize = ImGui::CalcTextSize(xorstr_("mlo-08122022"));
                 ImGui::SetCursorPos(ImVec2(buildInfoSize.x / 2 - buildStringSize.x / 2, buildInfoSize.y / 2 + style.ItemSpacing.y / 4));
-                ImGui::TextColored(StyleProvider::MottoColour, xorstr_("mlo-30112022"));
+                ImGui::TextColored(StyleProvider::MottoColour, xorstr_("mlo-08122022"));
                 ImGui::PopFont();
             }
             ImGui::EndChild();
