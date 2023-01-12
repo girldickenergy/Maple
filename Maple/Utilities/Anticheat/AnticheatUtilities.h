25d6135055b22d659e1a7c2e82c8d2c7988398ca
mrflashstudio <mrflashstudio@gmail.com>
2023-01-12 19:53:42 +0300

    add latest auth to the list of good known anticheats

--- Maple/Utilities/Anticheat/AnticheatUtilities.h
+++ Maple/Utilities/Anticheat/AnticheatUtilities.h
@@ -8,6 +8,7 @@ class AnticheatUtilities
 	static inline std::vector<std::string> goodKnownChecksums =
 	{
 		"7CA112211C73A956A80AF73358965BD2535FB579DF641504DA37D9BC92422DBC",
+		"3A1C15F9C776E2EEDCB7428D1B8B18F4B2C81BC4DC0221AB08A841A9A5328146"
 	};
 
 	static std::string getAnticheatChecksum();
