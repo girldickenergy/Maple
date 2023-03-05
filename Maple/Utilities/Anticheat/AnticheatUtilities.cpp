9bb941ae7c60f4f77c79935acbaec3dd55a43cb3
mrflashstudio <mrflashstudio@gmail.com>
2023-03-05 16:51:49 +0300

    AnticheatUtilities improvements
    - use md5 for auth checksum
    - exposed GetAnticheatChecksum
    - added an ability to read auth & osu binaries to a byte array

--- Maple/Utilities/Anticheat/AnticheatUtilities.cpp
+++ Maple/Utilities/Anticheat/AnticheatUtilities.cpp
@@ -4,12 +4,12 @@
 
 #include "files.h"
 #include "hex.h"
-#include "sha.h"
+#include "md5.h"
 #include "ThemidaSDK.h"
 
 #include "../Security/xorstr.hpp"
 
-std::string AnticheatUtilities::getAnticheatChecksum()
+std::string AnticheatUtilities::GetAnticheatChecksum()
 {
     STR_ENCRYPT_START
 	
@@ -18,17 +18,67 @@ std::string AnticheatUtilities::getAnticheatChecksum()
     const std::string::size_type pos = std::string(buffer).find_last_of(xorstr_("\\/"));
 
     const std::string osuAuthFilePath = std::string(buffer).substr(0, pos) + xorstr_("\\osu!auth.dll");
-
+	
     std::string checksum;
-    CryptoPP::SHA256 algo;
+    CryptoPP::MD5 algo;
     CryptoPP::FileSource fs(osuAuthFilePath.c_str(), true, new CryptoPP::HashFilter(algo, new CryptoPP::HexEncoder(new CryptoPP::StringSink(checksum))));
+
+    std::transform(checksum.begin(), checksum.end(), checksum.begin(), ::tolower);
 
     STR_ENCRYPT_END
 
     return checksum;
 }
 
+std::vector<uint8_t> AnticheatUtilities::GetAnticheatBytes()
+{
+    STR_ENCRYPT_START
+
+    char buffer[MAX_PATH];
+    GetModuleFileNameA(NULL, buffer, MAX_PATH);
+    const std::string::size_type pos = std::string(buffer).find_last_of(xorstr_("\\/"));
+
+    const std::string osuAuthFilePath = std::string(buffer).substr(0, pos) + xorstr_("\\osu!auth.dll");
+
+    std::vector<unsigned char> bytes;
+
+    std::ifstream file(osuAuthFilePath, std::ios::binary);
+
+    bytes.insert(bytes.begin(),
+        std::istreambuf_iterator<char>(file),
+        std::istreambuf_iterator<char>());
+
+    file.close();
+
+    STR_ENCRYPT_END
+
+    return bytes;
+}
+
+std::vector<uint8_t> AnticheatUtilities::GetGameBytes()
+{
+    STR_ENCRYPT_START
+
+    char buffer[MAX_PATH];
+    GetModuleFileNameA(NULL, buffer, MAX_PATH);
+    const std::string osuFilePath = std::string(buffer);
+
+    std::vector<unsigned char> bytes;
+
+    std::ifstream file(osuFilePath, std::ios::binary);
+
+    bytes.insert(bytes.begin(),
+        std::istreambuf_iterator<char>(file),
+        std::istreambuf_iterator<char>());
+
+    file.close();
+
+    STR_ENCRYPT_END
+
+    return bytes;
+}
+
 bool AnticheatUtilities::IsRunningGoodKnownVersion()
 {
-    return std::ranges::find(goodKnownChecksums, getAnticheatChecksum()) != goodKnownChecksums.end();
+    return std::ranges::find(goodKnownChecksums, GetAnticheatChecksum()) != goodKnownChecksums.end();
 }
--- Maple/Utilities/Anticheat/AnticheatUtilities.h
+++ Maple/Utilities/Anticheat/AnticheatUtilities.h
@@ -7,11 +7,12 @@ class AnticheatUtilities
 {
 	static inline std::vector<std::string> goodKnownChecksums =
 	{
-		"3A1C15F9C776E2EEDCB7428D1B8B18F4B2C81BC4DC0221AB08A841A9A5328146",
-		"48AA6F7ABA42FB16BA602FAB70A70A223EC6E7D757B7C2627C7CE00CBBEFE05B"
+		"284c1dc8a524e46fbe7a7cf846d3d9b1"
 	};
-
-	static std::string getAnticheatChecksum();
+	
+	static std::string GetAnticheatChecksum();
+	static std::vector<uint8_t> GetAnticheatBytes();
+	static std::vector<uint8_t> GetGameBytes();
 
 public:
 	static bool IsRunningGoodKnownVersion();
