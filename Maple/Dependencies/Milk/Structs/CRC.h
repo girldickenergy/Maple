491abf331e35d5534a48fcdb0236a6899626d665
Azuki <theorybreakerhacks@gmail.com>
2023-03-29 21:15:51 +0200

    Update CRC Struct

--- Maple/Dependencies/Milk/Structs/CRC.h
+++ Maple/Dependencies/Milk/Structs/CRC.h
@@ -1,13 +1,26 @@
-#pragma once
 
 struct CRC
 {
+    int unknown1;
+
     void* functionPointer;
+
+    int unknown2;
+
     int functionSize;
+
+    int unknown3;
+
     int checksum;
+
     int something1;
     int something2;
+    int something3;
+
     char className[256];
+
+    int unknown4;
+
     char functionName[256];
     CRC* nextEntry;
 };
