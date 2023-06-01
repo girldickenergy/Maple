8aaa7e4f7514239cef7050aa81dd0c0547c14926
Azuki <theorybreakerhacks@gmail.com>
2023-06-01 19:44:56 +0200

    Default opacity to 0 and update Transformation logic with osu's implementation

--- Maple/Features/ReplayEditor/Components/GameplayComponents/Drawable.cpp
+++ Maple/Features/ReplayEditor/Components/GameplayComponents/Drawable.cpp
@@ -11,7 +11,7 @@ Drawable::Drawable(DrawableType drawableType, int* timer, Vector2 position, Tran
 	_position = position;
 	_transformations = std::vector<Transformation>{ transformation };
 
-	_opacity = 1;
+	_opacity = 0.f;
 	_scale = 1;
 	_index = index;
 }
@@ -28,7 +28,7 @@ std::tuple<int, int> Drawable::GetDrawingTimes()
 				latestEndTime = transformation.GetEndTime();
 		}
 	}
-	return std::make_tuple(earliestStartTime, latestEndTime);
+	return std::make_tuple(earliestStartTime + 1, latestEndTime);
 }
 
 bool Drawable::NeedsToDraw()
@@ -45,42 +45,151 @@ void Drawable::PushTransformation(Transformation transformation)
 
 void Drawable::ApplyTransformation(Transformation transformation)
 {
-	if (transformation.GetStartTime() <= *_timer) {
-		int duration = transformation.GetEndTime() - transformation.GetStartTime();
-		float t = (static_cast<float>(*_timer) - static_cast<float>(transformation.GetStartTime())) / static_cast<float>(duration);
-		t = std::clamp(t, 0.f, 1.f);
+	int duration = transformation.GetEndTime() - transformation.GetStartTime();
+	float t = (static_cast<float>(*_timer) - static_cast<float>(transformation.GetStartTime())) / static_cast<float>(duration);
+	t = std::clamp(t, 0.f, 1.f);
 
-		switch (transformation.GetTransformationType())
-		{
-			case TransformationType::Fade:
-			{
-				float lerpedFloat = std::lerp(transformation.GetStartFloat(), transformation.GetEndFloat(), t);
-				_opacity = lerpedFloat;
-			}
-			break;
-			case TransformationType::Scale:
-			{
-				float lerpedFloat = std::lerp(transformation.GetStartFloat(), transformation.GetEndFloat(), t);
-				_scale = lerpedFloat;
-			}
-			break;
-			case TransformationType::Move:
-			{
-				float lerpedX = std::lerp(transformation.GetStartPosition().X, transformation.GetEndPosition().X, t);
-				float lerpedY = std::lerp(transformation.GetStartPosition().Y, transformation.GetEndPosition().Y, t);
-				_position = Vector2(lerpedX, lerpedY);
-			}
-			break;
-		}
+	switch (transformation.GetTransformationType())
+	{
+	case TransformationType::Fade:
+	{
+		float lerpedFloat = std::lerp(transformation.GetStartFloat(), transformation.GetEndFloat(), t);
+		_opacity = lerpedFloat;
+	}
+	break;
+	case TransformationType::Scale:
+	{
+		float lerpedFloat = std::lerp(transformation.GetStartFloat(), transformation.GetEndFloat(), t);
+		_scale = lerpedFloat;
+	}
+	break;
+	case TransformationType::Move:
+	{
+		float lerpedX = std::lerp(transformation.GetStartPosition().X, transformation.GetEndPosition().X, t);
+		float lerpedY = std::lerp(transformation.GetStartPosition().Y, transformation.GetEndPosition().Y, t);
+		_position = Vector2(lerpedX, lerpedY);
+	}
+	break;
 	}
 }
 
 void Drawable::DoTransformations()
 {
+	bool hasFade = false;
+	bool hasScale = false;
+	bool hasMove = false;
+
+	bool hasFuture = false;
+	bool hasPast = false;
+	bool shouldDraw = false;
+
 	for (auto& transformation : _transformations)
 	{
-		if (transformation.GetTransformationType() != TransformationType::Empty)
-			ApplyTransformation(transformation);
+		if (transformation.GetTransformationType() == TransformationType::Empty)
+			continue;
+
+		if (transformation.GetStartTime() >= *_timer || transformation.GetEndTime() > *_timer)
+		{
+			hasFuture = true;
+			if (transformation.GetStartTime() > *_timer)
+				continue;
+		}
+
+		if (transformation.GetEndTime() <= *_timer)
+		{
+			hasPast = true;
+			if (transformation.GetEndTime() < *_timer)
+				continue;
+		}
+
+		shouldDraw = true;
+
+		switch (transformation.GetTransformationType())
+		{
+			case TransformationType::Fade:
+				if (hasFade) 
+					break;
+				ApplyTransformation(transformation);
+				hasFade = true;
+				break;
+			case TransformationType::Scale:
+				if (hasScale)
+					break;
+				ApplyTransformation(transformation);
+				hasScale = true;
+				break;
+			case TransformationType::Move:
+				if (hasMove)
+					break;
+				ApplyTransformation(transformation);
+				hasMove = true;
+				break;
+		}
+	}
+
+	if ((!hasFuture && !shouldDraw) || (!(hasFuture && hasPast) && !shouldDraw))
+		return;
+
+	// Apply past transformations
+	for (auto it = _transformations.end() - 1; it != _transformations.begin(); --it)
+	{
+		auto transformation = *it;
+		if (transformation.GetEndTime() >= *_timer)
+			continue;
+
+		switch (transformation.GetTransformationType())
+		{
+			case TransformationType::Fade:
+				if (hasFade)
+					break;
+				_opacity = transformation.GetEndFloat();
+				hasFade = true;
+				break;
+			case TransformationType::Scale:
+				if (hasScale)
+					break;
+				_scale = transformation.GetEndFloat();
+				hasScale = true;
+				break;
+			case TransformationType::Move:
+				if (hasMove)
+					break;
+				_position = transformation.GetEndPosition();
+				hasMove = true;
+				break;
+		}
+	}
+
+	// Apply future transformations
+	if (hasFuture)
+	{
+		for (auto& transformation : _transformations)
+		{
+			if (transformation.GetStartTime() < *_timer)
+				continue;
+
+			switch (transformation.GetTransformationType())
+			{
+				case TransformationType::Fade:
+					if (hasFade)
+						break;
+					_opacity = transformation.GetStartFloat();
+					hasFade = true;
+					break;
+				case TransformationType::Scale:
+					if (hasScale)
+						break;
+					_scale = transformation.GetStartFloat();
+					hasScale = true;
+					break;
+				case TransformationType::Move:
+					if (hasMove)
+						break;
+					_position = transformation.GetStartPosition();
+					hasMove = true;
+					break;
+			}
+		}
 	}
 }
 
