07edaee1058b94d9b87c5b5c0c3a38bfb0d14288
mrflashstudio <mrflashstudio@gmail.com>
2023-08-05 20:49:42 +0300

    update version string

--- Maple/UI/Windows/MainMenu.cpp
+++ Maple/UI/Windows/MainMenu.cpp
@@ -177,9 +177,9 @@ void MainMenu::Render()
                 ImGui::PopFont();
 
                 ImGui::PushFont(StyleProvider::FontSmall);
-                const ImVec2 buildStringSize = ImGui::CalcTextSize(xorstr_("mlo-03082023"));
+                const ImVec2 buildStringSize = ImGui::CalcTextSize(xorstr_("mlo-05082023"));
                 ImGui::SetCursorPos(ImVec2(buildInfoSize.x / 2 - buildStringSize.x / 2, buildInfoSize.y / 2 + style.ItemSpacing.y / 4));
-                ImGui::TextColored(StyleProvider::MottoColour, xorstr_("mlo-03082023"));
+                ImGui::TextColored(StyleProvider::MottoColour, xorstr_("mlo-05082023"));
                 ImGui::PopFont();
             }
             ImGui::EndChild();
