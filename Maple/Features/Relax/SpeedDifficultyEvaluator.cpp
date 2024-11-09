85ed51109b9293775f5e7075128f73d85cc48763
mrflashstudio <mrflashstudio@gmail.com>
2024-11-09 15:58:23 +0300

    Simplify SpeedDifficultyEvaluator logic

--- Maple/Features/Relax/SpeedDifficultyEvaluator.cpp
+++ Maple/Features/Relax/SpeedDifficultyEvaluator.cpp
@@ -25,57 +25,26 @@ void SpeedDifficultyEvaluator::Initialize()
 		auto nextHitObject = i + 1 < hitObjectsCount ? HitObjectManager::GetHitObject(i + 1) : HitObject();
 
 		double strainTime = std::max(previousHitObject.IsNull ? 0 : currentHitObject.StartTime - previousHitObject.StartTime, MIN_DELTA_TIME);
-		currentStrain *= std::pow(1.0 - 0.5, strainTime / 1000.0 / 2.0);
 
-		if (currentHitObject.IsType(HitObjectType::Spinner))
-			strainValues.push_back(0);
-
-		double doubletapness = 1;
-
-		if (!nextHitObject.IsNull)
-		{
-			double currDeltaTime = std::max(1, previousHitObject.IsNull ? 0 : currentHitObject.StartTime - previousHitObject.StartTime);
-			double nextDeltaTime = std::max(1, nextHitObject.StartTime - currentHitObject.StartTime);
-			double deltaDifference = std::abs(nextDeltaTime - currDeltaTime);
-			double speedRatio = currDeltaTime / std::max(currDeltaTime, deltaDifference);
-			double windowRatio = std::pow(std::min(1.0, currDeltaTime / hitWindow300), 2);
-			doubletapness = std::pow(speedRatio, 1.0 - windowRatio);
-		}
+		currentStrain *= std::pow(1.0 - STRAIN_DECAY_BASE, strainTime / 1000.0);
 
 		strainTime /= std::clamp((strainTime / hitWindow300) / 0.93, 0.92, 1.0);
 
-		double speedBonus = 1.0;
+		double speedBonus = 0.0;
 
 		if (strainTime < MIN_SPEED_BONUS)
-			speedBonus = 1 + 0.75 * std::pow((MIN_SPEED_BONUS - strainTime) / SPEED_BALANCING_FACTOR, 2);
+			speedBonus = 0.75 * std::pow((MIN_SPEED_BONUS - strainTime) / SPEED_BALANCING_FACTOR, 2);
 
 		double distance = SINGLE_SPACING_THRESHOLD;
 		if (!previousHitObject.IsNull && !previousHitObject.IsType(HitObjectType::Spinner))
-		{
-			float scalingFactor = NORMALIZED_RADIUS / hitObjectRadius;
-			if (hitObjectRadius < 30.0)
-				scalingFactor *= 1.0f + std::min(30.0f - hitObjectRadius, 5.0f) / 50.0f;
+			distance = std::min(SINGLE_SPACING_THRESHOLD, static_cast<double>(previousHitObject.EndPosition.Distance(currentHitObject.Position)));
 
-			double travelDistance = 0.0;
-			if (currentHitObject.IsType(HitObjectType::Slider))
-				travelDistance = currentHitObject.SpatialLength * currentHitObject.SegmentCount * std::pow(currentHitObject.SegmentCount / 2.5, 1.0 / 2.5);
+		double distanceBonus = std::pow(distance / SINGLE_SPACING_THRESHOLD, 3.95) * DISTANCE_MULTIPLIER;
 
-			double minJumpDistance = (currentHitObject.Position * scalingFactor - previousHitObject.EndPosition * scalingFactor).Length();
+		double difficulty = (1.0 + speedBonus + distanceBonus) * 1000.0 / strainTime;
 
-			if (previousHitObject.IsType(HitObjectType::Slider))
-			{
-				double lastTravelTime = previousHitObject.Duration;
-				double minJumpTime = std::max(strainTime - lastTravelTime, static_cast<double>(MIN_DELTA_TIME));
-				float tailJumpDistance = (previousHitObject.EndPosition - currentHitObject.Position).Length() * scalingFactor;
-				minJumpDistance = std::max(0.0, std::min(minJumpDistance - (MAXIMUM_SLIDER_RADIUS - ASSUMED_SLIDER_RADIUS), tailJumpDistance - MAXIMUM_SLIDER_RADIUS));
-			}
-
-			distance = std::min(SINGLE_SPACING_THRESHOLD, travelDistance + minJumpDistance);
-		}
-
-		double difficulty = (speedBonus + speedBonus * std::pow(distance / SINGLE_SPACING_THRESHOLD, 3.5)) * doubletapness / strainTime;
-
-		currentStrain += difficulty * SPEED_SKILL_MULTIPLIER;
+		if (!currentHitObject.IsType(HitObjectType::Spinner))
+			currentStrain += difficulty * SPEED_SKILL_MULTIPLIER;
 
 		difficulties.push_back(difficulty);
 		strainValues.push_back(currentStrain);
--- Maple/Features/Relax/SpeedDifficultyEvaluator.h
+++ Maple/Features/Relax/SpeedDifficultyEvaluator.h
@@ -7,11 +7,9 @@ class SpeedDifficultyEvaluator
 	static inline constexpr double SINGLE_SPACING_THRESHOLD = 125.0;
 	static inline constexpr double MIN_SPEED_BONUS = 75.0;
 	static inline constexpr double SPEED_BALANCING_FACTOR = 40.0;
-	static inline constexpr int NORMALIZED_RADIUS = 50;
-	static inline constexpr double MAXIMUM_SLIDER_RADIUS = NORMALIZED_RADIUS * 2.4f;
-	static inline constexpr double ASSUMED_SLIDER_RADIUS = NORMALIZED_RADIUS * 1.8f;
+	static inline constexpr double DISTANCE_MULTIPLIER = 0.94;
 	static inline constexpr double SPEED_SKILL_MULTIPLIER = 1375.0;
-	static inline constexpr double STRAIN_DECAY_BASE = 0.5;
+	static inline constexpr double STRAIN_DECAY_BASE = 0.3;
 
 	static inline std::vector<double> difficulties;
 	static inline std::vector<double> difficultiesNormalized;
